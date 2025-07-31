9b6faefaed38abb7c142a11321c5172e059b8cca
Timothy Schoen <timschoen123@gmail.com>
2025-07-31 13:20:13 +0200

    Fix CLAP issue

--- build.py
+++ build.py
@@ -114,4 +114,7 @@ for plugin in plugins_config:
 
             plugin_filename = name + extension;
             os.makedirs(target_dir, exist_ok=True)
-            shutil.copytree(os.path.join(format_path, plugin_filename), os.path.join(target_dir, plugin_filename))
+            if os.path.isdir(src):
+                shutil.copytree(os.path.join(format_path, plugin_filename), os.path.join(target_dir, plugin_filename))
+            else
+                shutil.copy2(os.path.join(format_path, plugin_filename), os.path.join(target_dir, plugin_filename))
